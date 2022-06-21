248b4f289be2e0b7f48817a699e379cabe2f0461
Manuel Pineda <manuel.pineda@invictusbpo.com>
2022-06-20 22:42:38 -0600

    split routes

--- api/src/routes/v1/index.ts
+++ api/src/routes/v1/index.ts
@@ -1,9 +1,13 @@
 import {Router} from 'express';
 
+import users from './users';
+
 const routes = Router()
 
 routes.get('/', (req, res) => {
     res.send('hello world')
 })
+
+routes.use(users)
 
 export default routes
--- api/src/server.ts
+++ api/src/server.ts
@@ -3,12 +3,12 @@ import express, {Express} from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors'
 
+import {PORT} from 'config/contants';
 import routes from 'routes/v1';
 
 dotenv.config()
 
 const app: Express = express()
-const port = 5000
 
 app.use(cors())
 app.use(express.json())
@@ -16,6 +16,6 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(routes)
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
 })
