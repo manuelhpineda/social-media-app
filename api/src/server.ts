b20c45c152c4217445300281dd7d2f464a919ca6
Manuel Pineda <manuel.pineda@invictusbpo.com>
2022-06-20 22:32:47 -0600

    added server

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,9 @@
+import 'module-alias/register'
 import express, {Express} from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors'
+
+import routes from 'routes';
 
 dotenv.config()
 
@@ -11,9 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-    res.send("HELLO WORLD")
-})
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
