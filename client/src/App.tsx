44175ead40d09b6f5756da28f50206f9aeb296de
ravaxe33 <ravaxe33@gmail.com>
2022-06-23 19:52:04 -0600

    Added Login page using styled-components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
+import {FaFacebook, FaInstagram, FaTwitter} from 'react-icons/fa'
+import {MainContainer, InputContainer,ButtonContainer} from './components/Container'
+import {WelcomeText} from './components/Text'
+import {Input} from './components/Inputs'
+import {Button} from './components/Buttons'
+
 function App() {
   return (
-      <h1>Hello world</h1>
-  )
+    <MainContainer>
+      <WelcomeText>Welcome</WelcomeText>
+      <InputContainer>
+      <Input type="email" placeholder="Email Address"  />
+      <Input type="password" placeholder="Password" />
+      </InputContainer>
+      <ButtonContainer>
+        <Button>Login</Button>
+      </ButtonContainer>
+    </MainContainer>
+    )
 }
 
 export default App
-
---
+++ client/src/components/Text.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const WelcomeText = styled.h2`
+    margin: 3rem 0 2rem 0;
+
+`
+
+export const SignupWith = styled.h5`
+    cursor:pointer;
+    
+`
